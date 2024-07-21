ced7022f533da43824e29343cbcc4f944d214eaf
Artchem <chemerys.artem@gmail.com>
2024-07-21 20:17:05 +0200

    methodology page mobile

--- src/pages/OurMethodology/MethodologyList.tsx
+++ src/pages/OurMethodology/MethodologyList.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-export type MethodologyListProps = {};
+import { MethodologyListProps } from "@/types/MethotListProps";
 
-const MethodologyList: React.FC<MethodologyListProps> = () => {
-  return <li>Hello</li>;
+const MethodologyList: React.FC<MethodologyListProps> = ({
+  title,
+  description,
+}) => {
+  return (
+    <li key={title} className="">
+      <h3 className="text-lg font-extrabold text-[#FDAB0C] mb-2 ml-4">
+        {title}
+      </h3>
+      <p className="w-96 p-6 text-xs font-extrabold  rounded-3xl bg-gradient-to-r from-[#E3C319] to-[#C8AD1F]">
+        {description}
+      </p>
+    </li>
+  );
 };
 
 export default MethodologyList;
--- src/pages/OurMethodology/OurMethodology.tsx
+++ src/pages/OurMethodology/OurMethodology.tsx
@@ -1,24 +1,24 @@
-import { Image } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
 
 import metodologyImg from "../../image/metodology-img.png";
 
 import MethodologyList from "./MethodologyList";
+import { dataMethodology } from "./dataMethodology";
 
 import DefaultLayout from "@/layouts/default";
-import { title } from "@/components/primitives";
-
-// interface OurMethodologyPageProps {
-//   // Определите здесь пропсы, если они нужны
-// }
-// import type { ImageProps } from "@nextui-org/image";
-// const Image: React.FC<ImageProps> = Image as any;
 
 const OurMethodologyPage = () => {
+  const handleClick = () => {
+    console.log("Click button method");
+  };
+
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>Our Competitive Advantage</h1>
+          <h1 className="text-3xl font-extrabold text-[#FDAB0C] mb-2">
+            Our Competitive Advantage
+          </h1>
         </div>
         <Image
           // alt={t(`${data.title}`)}
@@ -26,14 +26,23 @@ const OurMethodologyPage = () => {
           className="w-full h-full object-contain"
           src={metodologyImg}
         />
-        {/* <img
-          alt="Our Methodology Diagram"
-          src={metodologyImg}
-          className="w-full h-full object-contain"
-        /> */}
-        <ul>
-          <MethodologyList />
+
+        <ul className="flex flex-col items-center justify-center gap-5 mb-20">
+          {dataMethodology.map((item) => {
+            return <MethodologyList key={item.title} {...item} />;
+          })}
         </ul>
+
+        <Button
+          className="min-w-64 mb-11 md:mb-0 rounded-3xl bg-gradient-to-r from-[#E3C319] to-[#C8AD1F"
+          onPress={handleClick}
+          type="button"
+          size="lg"
+          style={{ backgroundColor: "#AD8E20" }}
+        >
+          Contacte-nous
+          {/* {t("contactBtn1")} */}
+        </Button>
       </section>
     </DefaultLayout>
   );
