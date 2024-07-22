a3632cd63cbdd08ff29672c04fac1c0707ca48d3
Artchem <chemerys.artem@gmail.com>
2024-07-22 22:10:05 +0200

    add methodology page desktop

--- src/pages/OurMethodology/MethodologyList.tsx
+++ src/pages/OurMethodology/MethodologyList.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
-import { MethodologyListProps } from "@/types/MethotListProps";
+type MethodologyListProps = {
+  title: string;
+  description: string;
+};
 
 const MethodologyList: React.FC<MethodologyListProps> = ({
   title,
@@ -8,7 +11,7 @@ const MethodologyList: React.FC<MethodologyListProps> = ({
 }) => {
   return (
     <li key={title} className="">
-      <h3 className="text-lg font-extrabold text-[#FDAB0C] mb-2 ml-4">
+      <h3 className="text-lg font-extrabold text-center sm:text-start text-[#FDAB0C] mb-1 ml-4">
         {title}
       </h3>
       <p className="w-96 p-6 text-xs font-extrabold  rounded-3xl bg-gradient-to-r from-[#E3C319] to-[#C8AD1F]">
