a4c3b4fc2436aaf0bf0ec3b4f42a49ddf0a206a8
Artchem <chemerys.artem@gmail.com>
2024-07-23 17:59:50 +0200

    translate Contact page end Our Methodology page

--- src/i18n/locales/fr.ts
+++ src/i18n/locales/fr.ts
@@ -24,5 +24,34 @@ export const fr = {
 
   // footer
   privacyPolicy: "Politique de confidentialité",
-  informationOnPersonalDataProtection: "Information sur la protection des renseignements personnels"
+  informationOnPersonalDataProtection:
+    "Information sur la protection des renseignements personnels",
+
+  // contact us
+  inputName: "Nom complet",
+  inputMail: "Courriel",
+  inputTel: "Téléphone",
+  inputCom: "Commentaire",
+  contactBtn1: "Envoyer",
+
+  // Our Methodology
+  methodologyTitle: "Notre Avantage Concurrentiel",
+  methodologyBtn: "Connecté-nous",
+  methodologyText:
+    "Libérez-vous des contraintes administratives et concentrez-vous sur le développement de votre entreprise. Chez CommUnite, nous gérons vos recevables tout en créant un environnement financier optimal et sécurisé pour votre croissance, soutenu par une expertise juridique spécialisée.",
+  allInOneSolution: "Solution Tout-en-Un",
+  allInOneSolutionDescription:
+    "Thanks to our unique partnership with the law firm Maîtres du Recouvrement, we offer comprehensive management of your receivables' lifecycle - from legal oversight to receivables management and collection.",
+  cuttingEdgeTechnology: "Technologie de Pointe",
+  cuttingEdgeTechnologyDescription:
+    "Our automated systems and AI solutions optimize every step of the process, ensuring efficiency and accuracy.",
+  certifiedSecurity: "Sécurité Certifiée",
+  certifiedSecurityDescription:
+    "Avec nos solutions certifiées ISO 27001 et SOC 2 Type II, vos données sont protégées selon les plus hauts standards de l'industrie.",
+  integratedLegalExpertise: "Expertise Juridique Intégrée",
+  integratedLegalExpertiseDescription:
+    "L'implication directe d'avocats spécialisés garantit une approche légalement irréprochable à chaque étape.",
+  operationalCustomization: "Personnalisation Opérationnelle",
+  operationalCustomizationDescription:
+    "Nos processus agiles s'adaptent aux spécificités de votre entreprise, assurant une intégration fluide et une réactivité optimale face aux évolutions de votre environnement d'affaires. Nous optimisons continuellement nos méthodes pour répondre à vos besoins évolutifs, assurant ainsi une efficacité maximale et une pertinence constante de nos services.",
 };
