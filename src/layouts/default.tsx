a31a781946d706c319716d2d4dad5cff94fbe3b3
Artchem <chemerys.artem@gmail.com>
2024-07-28 23:03:21 +0200

    edit page for w4k size

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,7 +2,11 @@ import { Navbar } from "@/components/navbar";
 import Footer from "@/components/footer";
 import SwitcherBar from "@/components/switcherBar";
 
-export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const beforeElipsStyles =
     "before:content-[''] before:absolute xl:before:w-[750px] before:h-[550px] before:left-[50px] before:top-[-150px] before:rounded-full before:bg-gradient-to-b before:from-[#AC962244] before:to-[#44490600] before:blur-[100px] ";
 
@@ -17,7 +21,7 @@ export default function DefaultLayout({ children }: { children: React.ReactNode 
         <main className="m-auto w-[390px] md:w-full">
           <section className="w-[390px] md:w-auto flex flex-col items-center justify-center gap-4 py-10 md:py-5 ">
             {/* md:pl-12 lg:pr-28 */}
-            <div className="w4k:w-full w4k:flex-1 flex flex-col xl:flex-row items-center pl-0   pr-5  gap-10">
+            <div className="w4k:w-full w4k:flex-1 flex flex-col xl:flex-row items-center justify-center pl-0   pr-5  gap-10">
               {children}
             </div>
           </section>
--- src/pages/advantagesOfComuniti.tsx
+++ src/pages/advantagesOfComuniti.tsx
@@ -16,8 +16,9 @@ export default function AdvantagesOfCoMUnitiPage() {
   return (
     <DefaultLayout>
       {/* <div className="main-content w4k:w-full w4k:h-full flex flex-col items-center"> */}
-      <div className="first-part flex flex-col items-center">
-        <h1 className="text-3xl font-extrabold text-center text-[#FDAB0C] mb-6 mt-5 sm:mt-0">
+      {/* <div className="w4k:w-full w4k:h-full flex flex-col items-center"> */}
+      <div className="first-part flex flex-col items-center w4k:mr-24 ">
+        <h1 className="text-3xl font-extrabold text-center text-[#FDAB0C] mb-8 mt-5 sm:mt-0">
           {t("advantagesCommunitiTitle")}
         </h1>
 
@@ -25,10 +26,10 @@ export default function AdvantagesOfCoMUnitiPage() {
           <Image
             alt="Advantages of Communiti"
             width={485}
-            className="max-w-lg w-[245px] sm:w-[485px] sm:mb-5"
+            className="max-w-lg w-[245px] sm:w-[385px] lg:w-[485px] "
             src={metodologyImg}
           />
-          <p className="absolute top-[85px] left-[72px] sm:top-[175px] sm:left-[130px] w-28 z-10 text-[#A1A1AA] text-base sm:text-4xl font-bold ">
+          <p className="absolute top-[85px] left-[72px] sm:top-[145px] sm:left-[105px] lg:top-[175px] lg:left-[130px] w-28 z-10 text-[#A1A1AA] text-base sm:text-[28px] lg:text-4xl font-bold ">
             Advantages CommUnite
           </p>
         </div>
@@ -44,7 +45,7 @@ export default function AdvantagesOfCoMUnitiPage() {
       </div>
 
       <div className="second-part flex flex-col">
-        <ul className="flex flex-col items-center justify-center gap-3 sm:w-[400px] sm:mb-0 sm:mt-3">
+        <ul className="flex flex-col items-center justify-center gap-3 sm:w-[400px] md:w-[470px]  w4k:w-[600px] sm:mb-0 sm:mt-3">
           {advantagesComunitData.map(({ id, icon, text }) => {
             return (
               <li key={id} className="flex items-center gap-6 ">
@@ -54,7 +55,7 @@ export default function AdvantagesOfCoMUnitiPage() {
                   className="max-w-lg w-10 "
                   src={icon}
                 />
-                <p className="p-2 text-xs font-bold text-[#A1A1AA] ">
+                <p className="p-2 text-xs w4k:text-sm font-bold text-[#A1A1AA] ">
                   {t(`${text}`)}
                 </p>
               </li>
